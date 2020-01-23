7acbd7a06ad7a9d7ed5beb2b4f532379802c3088
Wes Cole <wesley.cole87@gmail.com>
2020-01-23 14:16:22 -0500

    Add hooks to index

--- packages/hooks/src/useIsMounted.ts
+++ packages/hooks/src/useIsMounted.ts
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export default function useIsMounted(): () => boolean {
+export function useIsMounted(): () => boolean {
   const mountedRef = React.useRef(false)
   const getIsMounted = React.useCallback(() => mountedRef.current, [])
 
