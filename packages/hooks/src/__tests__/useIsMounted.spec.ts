4a2ed9c6465c5191a58e31278ed74eca91c35477
Wes Cole <wesley.cole87@gmail.com>
2020-01-23 14:05:43 -0500

    Add useIsMounted hook

---
+++ packages/hooks/src/__tests__/useIsMounted.spec.ts
@@ -0,0 +1,28 @@
+import { renderHook } from '@testing-library/react-hooks'
+import useIsMounted from '../useIsMounted'
+
+describe('useIsMounted', () => {
+  it('should be defined', () => {
+    expect(useIsMounted).toBeDefined()
+  })
+
+  it('should return a function', () => {
+    const hook = renderHook(() => useIsMounted(), { initialProps: false })
+
+    expect(typeof hook.result.current).toEqual('function')
+  })
+
+  it('should return true if component is mounted', () => {
+    const hook = renderHook(() => useIsMounted(), { initialProps: false })
+
+    expect(hook.result.current()).toBeTruthy()
+  })
+
+  it('should return false if component is unmounted', () => {
+    const hook = renderHook(() => useIsMounted(), { initialProps: false })
+
+    hook.unmount()
+
+    expect(hook.result.current()).toBeFalsy()
+  })
+})
---
+++ packages/hooks/src/useIsMounted.ts
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+export default function useIsMounted(): () => boolean {
+  const mountedRef = React.useRef(false)
+  const getIsMounted = React.useCallback(() => mountedRef.current, [])
+
+  React.useEffect(() => {
+    mountedRef.current = true
+
+    return () => {
+      mountedRef.current = false
+    }
+  })
+
+  return getIsMounted
+}
