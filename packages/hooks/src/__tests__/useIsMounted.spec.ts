33f74380680193f83e5192aed67a1c7d39f12107
Wes Cole <wesley.cole87@gmail.com>
2020-01-23 14:18:58 -0500

    Update import to named import for useIsMounted

--- packages/hooks/src/__tests__/useIsMounted.spec.ts
+++ packages/hooks/src/__tests__/useIsMounted.spec.ts
@@ -1,5 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks'
-import useIsMounted from '../useIsMounted'
+import { useIsMounted } from '../useIsMounted'
 
 describe('useIsMounted', () => {
   it('should be defined', () => {
