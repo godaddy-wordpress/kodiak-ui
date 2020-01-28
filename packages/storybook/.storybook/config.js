de273c820019ad8aa49be4f028bbed6f51dfde7c
Wes Cole <wesley.cole87@gmail.com>
2020-01-28 11:42:09 -0500

    update storybook and github pr template

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,5 +1,10 @@
-import { configure } from '@storybook/react'
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import { ThemeProvider } from 'theme-ui'
+import { theme } from '../src/theme'
 
 const req = require.context('../src', true, /.stories.(ts|js)x?$/)
+
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>)
 
 configure(req, module)
