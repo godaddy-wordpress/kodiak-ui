7120a16c44a9e3211f8826b07acd497f72b26896
Wes Cole <wesley.cole87@gmail.com>
2020-01-29 12:38:34 -0500

    Add test stubs for Box

---
+++ packages/box/src/__tests__/Box.spec.tsx
@@ -0,0 +1,9 @@
+describe('Box', () => {
+  it('should render the Box as a div element', () => {})
+
+  it('should render the Box as a main element', () => {})
+
+  it('should style the element with the `sx` prop', () => {})
+
+  it('should style the element with styled-system props', () => {})
+})
