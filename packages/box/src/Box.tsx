9dd9d0bcc04a0de2b9715ba431698ef7c383e43d
Wes Cole <wesley.cole87@gmail.com>
2020-01-29 01:07:47 -0500

    Add BoxProps interface

--- packages/box/src/Box.tsx
+++ packages/box/src/Box.tsx
@@ -1,9 +1,10 @@
 /** @jsx jsx */
+
 import styled from '@emotion/styled'
 import { css, jsx } from 'theme-ui'
 import { SerializedStyles } from '@emotion/serialize'
 import { createShouldForwardProp } from '@styled-system/should-forward-prop'
-import { space, color } from 'styled-system'
+import { space, color, SpaceProps, ColorProps } from 'styled-system'
 
 const shouldForwardProp = createShouldForwardProp([
   ...(space.propNames as string[]),
@@ -14,9 +15,16 @@ const sx = (props: any): SerializedStyles => {
   return css(props.sx)(props.theme)
 }
 
-export const Box = styled(({ as: T = 'div', ...props }) => <T {...props} />, {
-  shouldForwardProp,
-})(
+interface BoxProps extends SpaceProps, ColorProps, JSX.IntrinsicAttributes {
+  as: string
+}
+
+export const Box = styled(
+  ({ as: T = 'div', ...props }: BoxProps) => <T {...props} />,
+  {
+    shouldForwardProp,
+  },
+)(
   {
     boxSizing: 'border-box',
     margin: 0,
