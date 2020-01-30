c2669461a58a83a9ee121ba3f0e7b917653932a4
Wes Cole <wesley.cole87@gmail.com>
2020-01-30 15:27:33 -0500

    Add basic storybook story for flex

---
+++ packages/storybook/src/Flex/Flex.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Flex } from '@kodiak/flex'
+
+export default { title: 'Flex' }
+
+export function initial() {
+  return <Flex>Basic Box that renders a Div</Flex>
+}
