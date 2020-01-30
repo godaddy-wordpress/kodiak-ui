42c2895d6e90f31b64c173e93c196ef088f15f6c
Wes Cole <wesley.cole87@gmail.com>
2020-01-30 15:29:43 -0500

    Add test for Flex

---
+++ packages/flex/src/__tests__/Flex.spec.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react'
+import serializer from 'jest-emotion'
+import renderer from 'react-test-renderer'
+import { Flex } from '../Flex'
+
+expect.addSnapshotSerializer(serializer)
+
+describe('Box', () => {
+  it('should render the Flex as a div element', () => {
+    expect(renderer.create(<Flex>Rendering Flex element</Flex>).toJSON())
+      .toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+      }
+
+      <div
+        className="emotion-0"
+      >
+        Rendering Flex element
+      </div>
+    `)
+  })
+
+  it('should style the element with the `sx` prop', () => {
+    expect(
+      renderer
+        .create(<Flex sx={{ color: 'white' }}>Rendering div element</Flex>)
+        .toJSON(),
+    ).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        color: white;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+      }
+
+      <div
+        className="emotion-0"
+      >
+        Rendering div element
+      </div>
+    `)
+  })
+
+  it('should style the element with styled-system props', () => {
+    expect(
+      renderer
+        .create(<Flex alignItems="center">Rendering div element</Flex>)
+        .toJSON(),
+    ).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+      }
+
+      <div
+        className="emotion-0"
+      >
+        Rendering div element
+      </div>
+    `)
+  })
+})
