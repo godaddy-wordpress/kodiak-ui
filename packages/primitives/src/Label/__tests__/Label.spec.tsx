cc100c070c74758bc2fab11dabcdd4c46f1c1228
Alex Chan <alexander.m.chan@gmail.com>
2020-01-31 16:19:33 -0700

    Update test

--- packages/primitives/src/Label/__tests__/Label.spec.tsx
+++ packages/primitives/src/Label/__tests__/Label.spec.tsx
@@ -24,52 +24,34 @@ describe('Label', () => {
     `)
   })
 
-  it('should style the element with the `sx` prop', () => {
-    expect(
-      renderer
-        .create(
-          <Label sx={{ color: 'white' }}>Rendering with an sx prop</Label>,
-        )
-        .toJSON(),
-    ).toMatchInlineSnapshot(`
-      .emotion-0 {
-        box-sizing: border-box;
-        margin: 0;
-        min-width: 0;
-        color: white;
-      }
-
-      <label
-        className="emotion-0"
-      >
-        Rendering with an sx prop
-      </label>
-    `)
-  })
-
   it('should style the element with styled-system props and accept label props', () => {
     expect(
       renderer
         .create(
-          <Label htmlFor="myinput" fontWeight="bold">
-            Rendering bold p tag
+          <Label htmlFor="myinput" fontWeight="bold" sx={{ color: 'white' }}>
+            Rendering bold
             <input id="myinput" />
           </Label>,
         )
         .toJSON(),
     ).toMatchInlineSnapshot(`
-      .emotion-0 {
-        box-sizing: border-box;
-        margin: 0;
-        min-width: 0;
-        font-weight: bold;
-      }
+        .emotion-0 {
+          box-sizing: border-box;
+          margin: 0;
+          min-width: 0;
+          font-weight: bold;
+          color: white;
+        }
 
-      <label
-        className="emotion-0"
-      >
-        Rendering bold p tag
-      </label>
+        <label
+          className="emotion-0"
+          htmlFor="myinput"
+        >
+          Rendering bold
+          <input
+            id="myinput"
+          />
+        </label>
     `)
   })
 })
