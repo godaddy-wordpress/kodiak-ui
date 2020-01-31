afa337ae7ca8f7d56058fadbe2eab37880b6356e
Alex Chan <alexander.m.chan@gmail.com>
2020-01-31 12:19:37 -0700

    Add storybook stories

---
+++ packages/storybook/src/Link/Link.stories.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { Link } from '@kodiak/primitives'
+
+export default { title: 'Link' }
+
+export function initial() {
+  return (
+    <Link href="https://jilt.com" target="_blank" rel="noreferrer">
+      jilt.com
+    </Link>
+  )
+}
+
+export function withHover() {
+  return (
+    <Link
+      href="https://jilt.com"
+      color="red"
+      sx={{ '&:hover': { color: 'blue' } }}
+      target="_blank"
+      rel="noreferrer"
+    >
+      jilt.com
+    </Link>
+  )
+}
