9f9399891acea698685378cfd0d098776b358f90
Wesley Cole <wesley.cole87@gmail.com>
2020-01-31 15:44:56 -0500

    Create the Text component (#11)
    * Update package.json for publish
    
    * Add Text primitive
    
    * Export variant and sx functions from Box
    
    * Add exports to index
    
    * Add storybook story for Text
    
    * Add tests for Text
    
    * Update theme value for heading

---
+++ packages/primitives/src/Text/__tests__/Text.spec.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react'
+import serializer from 'jest-emotion'
+import renderer from 'react-test-renderer'
+import { Text } from '../Text'
+
+expect.addSnapshotSerializer(serializer)
+
+describe('Text', () => {
+  it('should render the Text as a p element', () => {
+    expect(renderer.create(<Text>Rendering p element</Text>).toJSON())
+      .toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+      }
+
+      <p
+        className="emotion-0"
+      >
+        Rendering p element
+      </p>
+    `)
+  })
+
+  it('should render the Text component as a span element', () => {
+    expect(
+      renderer.create(<Text as="span">Rendering span element</Text>).toJSON(),
+    ).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+      }
+
+      <span
+        className="emotion-0"
+      >
+        Rendering span element
+      </span>
+    `)
+  })
+
+  it('should style the element with the `sx` prop', () => {
+    expect(
+      renderer
+        .create(<Text sx={{ color: 'white' }}>Rendering p with sx prop</Text>)
+        .toJSON(),
+    ).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        color: white;
+      }
+
+      <p
+        className="emotion-0"
+      >
+        Rendering p with sx prop
+      </p>
+    `)
+  })
+
+  it('should style the element with styled-system props', () => {
+    expect(
+      renderer
+        .create(<Text fontWeight="bold">Rendering bold p tag</Text>)
+        .toJSON(),
+    ).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        font-weight: bold;
+      }
+
+      <p
+        className="emotion-0"
+      >
+        Rendering bold p tag
+      </p>
+    `)
+  })
+})
