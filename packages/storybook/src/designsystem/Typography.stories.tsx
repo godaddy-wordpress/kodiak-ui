cc0bc41bd70e580a1126248883fdd859d0a3f6ce
Alex Chan <alexander.m.chan@gmail.com>
2020-02-01 15:41:59 -0700

    Move to designsystem folder

--- packages/storybook/src/designsystem/Typography.stories.tsx
+++ packages/storybook/src/designsystem/Typography.stories.tsx
@@ -2,10 +2,14 @@ import * as React from 'react'
 import { Box, Text, Flex } from '@kodiak/primitives'
 import { theme } from '../theme'
 
+export default {
+  title: 'Design System/Typography',
+}
+
 type TypographyExampleProps = React.PropsWithChildren<{
   size: number
 }>
-export function Typography() {
+export function Intro() {
   return (
     <Text p={4}>
       Typography is the art and technique of arranging type to make written
@@ -18,14 +22,19 @@ export function FontFamily() {
   return (
     <Flex flexDirection="column">
       <Box color="grey.6">
-        {Object.keys(theme.fonts).map((fontsKey, index) => (
-          <Text key={index} fontFamily={theme.fonts[fontsKey]}>
-            {fontsKey}{' '}
-            <Text as="span" color="grey.4">
-              ({theme.fonts[fontsKey]})
+        {Object.keys(theme.fonts).map((fontsKey, index) => {
+          const fontFamily =
+            theme.fonts[fontsKey as keyof typeof theme['fonts']]
+
+          return (
+            <Text key={index} fontFamily={fontFamily}>
+              {fontsKey}{' '}
+              <Text as="span" color="grey.4">
+                ({fontFamily})
+              </Text>
             </Text>
-          </Text>
-        ))}
+          )
+        })}
       </Box>
     </Flex>
   )
@@ -35,14 +44,21 @@ export function FontWeight() {
   return (
     <Flex flexDirection="column">
       <Box color="grey.6">
-        {Object.keys(theme.fontWeights).map((fontWeightKey, index) => (
-          <Text key={index} fontWeight={theme.fontWeights[fontWeightKey]}>
-            {fontWeightKey}{' '}
-            <Text as="span" color="grey.4">
-              ({theme.fontWeights[fontWeightKey]})
+        {Object.keys(theme.fontWeights).map((fontWeightKey, index) => {
+          const fontWeight =
+            theme.fontWeights[
+              fontWeightKey as keyof typeof theme['fontWeights']
+            ]
+
+          return (
+            <Text key={index} fontWeight={fontWeight}>
+              {fontWeightKey}{' '}
+              <Text as="span" color="grey.4">
+                ({fontWeight})
+              </Text>
             </Text>
-          </Text>
-        ))}
+          )
+        })}
       </Box>
     </Flex>
   )
@@ -78,8 +94,4 @@ export function Sizing() {
       </Box>
     </Flex>
   )
-}
-
-export default {
-  title: 'Design System/Typography',
 }
