ed5b507741408870cd1b0c7ad1cfddfbaeb10c94
Alex Chan <alexander.m.chan@gmail.com>
2020-02-01 15:28:44 -0700

    Sort storybook stories

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import { configure, addDecorator } from '@storybook/react'
+import { configure, addDecorator, addParameters } from '@storybook/react'
 import { ThemeProvider } from 'theme-ui'
 import { theme } from '../src/theme'
 
 const req = require.context('../src', true, /.stories.(ts|js)x?$/)
+
+addParameters({
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
+})
 
 addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>)
 
