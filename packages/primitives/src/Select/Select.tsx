96dae920da77c735ba3568a6fdbfcd83ab4d7299
Wes Cole <wesley.cole87@gmail.com>
2020-02-04 13:33:36 -0500

    Finish the Select component

--- packages/primitives/src/Select/Select.tsx
+++ packages/primitives/src/Select/Select.tsx
@@ -22,8 +22,6 @@ import { Flex } from '../Flex'
  */
 export const baseStyles = ({ theme }: { theme: Theme }): SerializedStyles =>
   css({
-    alignSelf: 'flex-start',
-    display: 'block',
     width: '100%',
     p: 2,
     appearance: 'none',
@@ -60,13 +58,31 @@ export const SelectStyled = styled<'select', SelectProps>('select', {
   sx,
 )
 
+function SelectDropdownIcon() {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      height="16"
+      width="16"
+      style={{
+        alignSelf: 'center',
+        display: 'block',
+        marginLeft: '-28px',
+      }}
+    >
+      <path
+        d="M11.912 5.754a.62.62 0 0 0-.25-.186.883.883 0 0 0-.344-.068H4.682c-.12 0-.24.024-.344.068a.62.62 0 0 0-.25.186.398.398 0 0 0-.088.252.405.405 0 0 0 .098.25l3.318 4.004c.061.073.147.134.249.176A.886.886 0 0 0 8 10.5a.886.886 0 0 0 .335-.064.633.633 0 0 0 .249-.176l3.318-4.004a.405.405 0 0 0 .098-.25.398.398 0 0 0-.088-.252z"
+        fill="black"
+      />
+    </svg>
+  )
+}
+
 export const Select = React.forwardRef(
   (props: SelectProps, ref: React.Ref<HTMLSelectElement>) => (
     <Flex>
       <SelectStyled ref={ref} {...props} />
-      <svg style={{ marginLeft: '-28px', display: 'block' }}>
-        <path d="M7 10l5 5 5-5z" />
-      </svg>
+      <SelectDropdownIcon />
     </Flex>
   ),
 )
