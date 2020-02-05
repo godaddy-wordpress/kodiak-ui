b47ab4ce4c100890104e7fbae3d2afa096e7bb17
Alex Chan <alexander.m.chan@gmail.com>
2020-02-04 20:42:26 -0700

    Increase before unload coverage

--- packages/hooks/src/__tests__/useBeforeUnload.spec.ts
+++ packages/hooks/src/__tests__/useBeforeUnload.spec.ts
@@ -3,9 +3,10 @@ import { useBeforeUnload } from '../useBeforeUnload'
 
 describe('useBeforeUnload', () => {
   let addEventListenerSpy: jest.SpyInstance
-
+  let removeEventListenerSpy: jest.SpyInstance
   beforeEach(() => {
     addEventListenerSpy = jest.spyOn(window, 'addEventListener')
+    removeEventListenerSpy = jest.spyOn(window, 'addEventListener')
   })
 
   afterEach(() => {
@@ -15,5 +16,10 @@ describe('useBeforeUnload', () => {
   it('should add event listener', () => {
     renderHook(() => useBeforeUnload({ enabled: true }))
     expect(addEventListenerSpy).toBeCalled()
+  })
+
+  it('should remove event listener', () => {
+    renderHook(() => useBeforeUnload({ enabled: false }))
+    expect(removeEventListenerSpy).toBeCalled()
   })
 })
