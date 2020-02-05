1681f19bf99b2522251ac6989f761a58914ccf21
Wes Cole <wesley.cole87@gmail.com>
2020-02-05 13:07:46 -0500

    Update snapshot

--- packages/primitives/src/Select/__tests__/Select.spec.tsx
+++ packages/primitives/src/Select/__tests__/Select.spec.tsx
@@ -19,7 +19,7 @@ describe('Select', () => {
         )
         .toJSON(),
     ).toMatchInlineSnapshot(`
-      .emotion-1 {
+      .emotion-2 {
         box-sizing: border-box;
         margin: 0;
         min-width: 0;
@@ -46,8 +46,24 @@ describe('Select', () => {
         background-color: transparent;
       }
 
+      .emotion-1 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        fill: currentColor;
+        color: black;
+        display: block;
+        margin-left: -28px;
+        color: text;
+        height: 16px;
+        width: 16px;
+        -webkit-align-self: center;
+        -ms-flex-item-align: center;
+        align-self: center;
+      }
+
       <div
-        className="emotion-1"
+        className="emotion-2"
       >
         <select
           className="emotion-0"
@@ -67,20 +83,13 @@ describe('Select', () => {
           </option>
         </select>
         <svg
-          height="16"
-          style={
-            Object {
-              "alignSelf": "center",
-              "display": "block",
-              "marginLeft": "-28px",
-            }
-          }
+          className="emotion-1"
           viewBox="0 0 16 16"
-          width="16"
         >
+          <title />
+          <desc />
           <path
             d="M11.912 5.754a.62.62 0 0 0-.25-.186.883.883 0 0 0-.344-.068H4.682c-.12 0-.24.024-.344.068a.62.62 0 0 0-.25.186.398.398 0 0 0-.088.252.405.405 0 0 0 .098.25l3.318 4.004c.061.073.147.134.249.176A.886.886 0 0 0 8 10.5a.886.886 0 0 0 .335-.064.633.633 0 0 0 .249-.176l3.318-4.004a.405.405 0 0 0 .098-.25.398.398 0 0 0-.088-.252z"
-            fill="black"
           />
         </svg>
       </div>
