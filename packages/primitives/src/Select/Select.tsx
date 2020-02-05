4009222de2dd5576b41b13583a8391c8a4637b5a
Wes Cole <wesley.cole87@gmail.com>
2020-02-05 11:14:45 -0500

    Use primary color for dropdown icon

--- packages/primitives/src/Select/Select.tsx
+++ packages/primitives/src/Select/Select.tsx
@@ -8,14 +8,13 @@ import {
   sx,
   shouldForwardProp,
   styledSystemProps,
-  Box,
 } from '../Box/Box'
 import { Flex } from '../Flex'
 
 /**
  * base
  *
- * Generate the base CSS for the styled component
+ * Generate the base CSS for the Select component
  * that is aware of the Theme UI theme
  *
  * @param props
@@ -68,7 +67,7 @@ function SelectDropdownIcon() {
     >
       <path
         d="M11.912 5.754a.62.62 0 0 0-.25-.186.883.883 0 0 0-.344-.068H4.682c-.12 0-.24.024-.344.068a.62.62 0 0 0-.25.186.398.398 0 0 0-.088.252.405.405 0 0 0 .098.25l3.318 4.004c.061.073.147.134.249.176A.886.886 0 0 0 8 10.5a.886.886 0 0 0 .335-.064.633.633 0 0 0 .249-.176l3.318-4.004a.405.405 0 0 0 .098-.25.398.398 0 0 0-.088-.252z"
-        fill="black"
+        fill="primary"
       />
     </svg>
   )
