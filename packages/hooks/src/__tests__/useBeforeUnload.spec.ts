b78ee687ccf124c43751e9ab1eb768f21dc156a8
Alex Chan <alexander.m.chan@gmail.com>
2020-02-04 21:39:23 -0700

    Fix test coverage

--- packages/hooks/src/__tests__/useBeforeUnload.spec.ts
+++ packages/hooks/src/__tests__/useBeforeUnload.spec.ts
@@ -17,11 +17,33 @@ describe('useBeforeUnload', () => {
   it('should add event listener', () => {
     renderHook(() => useBeforeUnload({ enabled: true }))
     expect(addEventListenerSpy).toBeCalled()
+
+    const addBeforeUnloadCalls = addEventListenerSpy.mock.calls.filter(
+      calledFn => {
+        return calledFn[0] === 'beforeunload'
+      },
+    )
+    expect(addBeforeUnloadCalls.length).toBe(1)
+
+    const mockOnBeforeUnloadEvent = {
+      preventDefault: jest.fn(),
+    }
+
+    const preventUnload = addBeforeUnloadCalls[0][1]
+    preventUnload(mockOnBeforeUnloadEvent)
+
+    expect(mockOnBeforeUnloadEvent.preventDefault).toBeCalledTimes(1)
   })
 
   it('should remove event listener', () => {
     expect(removeEventListenerSpy).toBeCalledTimes(0)
     renderHook(() => useBeforeUnload({ enabled: false }))
-    expect(removeEventListenerSpy).toBeCalled()
+
+    const removeBeforeUnloadCalls = removeEventListenerSpy.mock.calls.filter(
+      calledFn => {
+        return calledFn[0] === 'beforeunload'
+      },
+    )
+    expect(removeBeforeUnloadCalls.length).toBe(1)
   })
 })
