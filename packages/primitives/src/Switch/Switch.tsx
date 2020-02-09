a8205f81018b820cb8946e2100e2a1c79aef27bb
Alex Chan <alexander.m.chan@gmail.com>
2020-02-09 16:56:16 -0700

    Refactor indicator

--- packages/primitives/src/Switch/Switch.tsx
+++ packages/primitives/src/Switch/Switch.tsx
@@ -12,33 +12,29 @@ interface SwitchProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 type SwitchIndicatorProps = {
   checked?: boolean
-  disabled?: boolean
+  hasLabel?: boolean
 } & Pick<SwitchProps, 'sx' | 'variant' | 'variantKey'>
 
 function SwitchIndicator({
   checked,
-  disabled,
   sx,
   variant,
   variantKey,
+  hasLabel,
 }: SwitchIndicatorProps) {
   return (
     <Box
       variant={`${variant}.${checked ? 'checked' : 'unchecked'}`}
       variantKey={variantKey}
-      as="span"
+      display="inline-flex"
+      width={28}
+      height={16}
+      borderRadius="full"
       borderStyle="solid"
       borderWidth={1}
-      opacity={disabled ? 0.7 : 1}
-      borderRadius={40}
-      display="inline-block"
-      height={16}
-      ml={-38}
-      mr={2}
-      position="relative"
+      mr={hasLabel ? 2 : 0}
       sx={{
         transition: 'background-color 0.1s cubic-bezier(0.4, 1, 0.75, 0.9)',
-        ...(disabled && { cursor: 'not-allowed', outline: 'none' }),
 
         'input:focus ~ &': {
           outlineWidth: 2,
@@ -46,71 +42,73 @@ function SwitchIndicator({
           outlineOffset: 2,
         },
 
-        '&::before': {
-          borderRadius: '50%',
-          content: '""',
-          display: 'block',
-          height: 12,
-          width: 12,
-          position: 'absolute',
-          top: '50%',
-          left: checked ? 'calc(100% - 13px)' : '1px',
-          transform: 'translateY(-50%)',
-          transition: 'left 0.1s cubic-bezier(0.4, 1, 0.75, 0.9)',
-        },
         ...sx,
       }}
-      width={28}
-    />
+      alignItems="center"
+    >
+      {/* Circle Indicator */}
+      <Box
+        width={12}
+        height={12}
+        borderRadius="full"
+        backgroundColor="white"
+        ml="1px"
+        sx={{
+          transform: `translateX(${checked ? 'calc(100% - 1px)' : '0'})`,
+          transition: 'transform 0.1s cubic-bezier(0.4, 1, 0.75, 0.9)',
+        }}
+      />
+    </Box>
   )
 }
 
 export const Switch = React.forwardRef(
   (
     {
-      label,
       checked,
+      label,
+      onChange,
       disabled,
-      children,
+      sx,
       variantKey = 'forms',
       variant = 'switch',
-      sx,
-      ...rest
+      children,
+      ...props
     }: SwitchProps,
     ref: React.Ref<HTMLInputElement>,
-  ) => (
-    <Label
-      alignItems="center"
-      display="inline-flex"
-      flexShrink={0}
-      sx={{
-        cursor: disabled ? 'not-allowed' : 'pointer',
-        textTransform: 'none',
-      }}
-      opacity={disabled ? 0.5 : 1}
-      pl={38}
-      fontSize="default"
-      position="relative"
-    >
-      <Input
-        ref={ref}
-        type="checkbox"
-        position="absolute"
-        opacity={0}
-        left={0}
-        top={0}
-        zIndex={-1}
-        checked={checked}
-        {...rest}
-      />
-      <SwitchIndicator
-        checked={checked}
-        disabled={disabled}
-        variant={variant}
-        variantKey={variantKey}
-        sx={sx}
-      />
-      {label || children}
-    </Label>
-  ),
+  ) => {
+    return (
+      <Label
+        alignItems="center"
+        display="inline-flex"
+        opacity={disabled ? 0.5 : 1}
+        sx={{
+          cursor: disabled ? 'not-allowed' : 'pointer',
+        }}
+        position="relative"
+      >
+        <Input
+          ref={ref}
+          opacity={0}
+          type="checkbox"
+          position="absolute"
+          left={0}
+          top={0}
+          zIndex={1}
+          checked={checked}
+          onChange={onChange}
+          disabled={disabled}
+          {...props}
+        />
+        <SwitchIndicator
+          checked={checked}
+          variant={variant}
+          variantKey={variantKey}
+          sx={sx}
+          hasLabel={!!label}
+        />
+        {label || children}
+      </Label>
+    )
+  },
 )
