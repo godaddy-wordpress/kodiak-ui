3a91a26622a1de216f7c2d5ac1a101143fa827c0
Alex Chan <alexander.m.chan@gmail.com>
2020-02-10 08:36:49 -0700

    Update snapshot

--- packages/primitives/src/Switch/__tests__/Switch.spec.tsx
+++ packages/primitives/src/Switch/__tests__/Switch.spec.tsx
@@ -24,30 +24,21 @@ describe('Switch', () => {
   it('should render the switch', () => {
     expect(renderer.create(<Switch label="Toggle this"></Switch>).toJSON())
       .toMatchInlineSnapshot(`
-      .emotion-2 {
+      .emotion-3 {
         box-sizing: border-box;
         margin: 0;
         min-width: 0;
-        padding-left: 128px;
-        margin-right: 32px;
-        margin-bottom: 32px;
         opacity: 1;
-        font-size: default;
         display: -webkit-inline-box;
         display: -webkit-inline-flex;
         display: -ms-inline-flexbox;
         display: inline-flex;
-        min-height: none;
         -webkit-align-items: center;
         -webkit-box-align: center;
         -ms-flex-align: center;
         align-items: center;
-        -webkit-flex-shrink: 0;
-        -ms-flex-negative: 0;
-        flex-shrink: 0;
         position: relative;
         cursor: pointer;
-        text-transform: none;
       }
 
       .emotion-0 {
@@ -70,59 +61,68 @@ describe('Switch', () => {
         position: absolute;
         left: 0;
         top: 0;
-        z-index: -1;
+        z-index: 1;
+      }
+
+      .emotion-2 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        margin-right: 8px;
+        display: -webkit-inline-box;
+        display: -webkit-inline-flex;
+        display: -ms-inline-flexbox;
+        display: inline-flex;
+        width: 28px;
+        height: 16px;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        border-radius: full;
+        border-style: solid;
+        border-width: 1px;
+        -webkit-transition: background-color 0.1s cubic-bezier(0.4,1,0.75,0.9);
+        transition: background-color 0.1s cubic-bezier(0.4,1,0.75,0.9);
+      }
+
+      input:focus ~ .emotion-2 {
+        outline-width: 2px;
+        outline-style: solid;
+        outline-offset: 2px;
       }
 
       .emotion-1 {
         box-sizing: border-box;
         margin: 0;
         min-width: 0;
-        margin-left: -38px;
-        margin-right: 8px;
-        opacity: 1;
-        display: inline-block;
-        height: 16px;
-        width: 28px;
-        border-style: solid;
-        border-width: 1px;
-        border-radius: 40px;
-        position: relative;
-        -webkit-transition: background-color 0.1s cubic-bezier(0.4,1,0.75,0.9);
-        transition: background-color 0.1s cubic-bezier(0.4,1,0.75,0.9);
-      }
-
-      input:focus ~ .emotion-1 {
-        outline-width: 2px;
-        outline-style: solid;
-        outline-offset: 2px;
-      }
-
-      .emotion-1::before {
-        border-radius: 50%;
-        content: "";
-        display: block;
-        height: 12px;
+        margin-left: 1px;
+        background-color: background;
         width: 12px;
-        position: absolute;
-        top: 50%;
-        left: 1px;
-        -webkit-transform: translateY(-50%);
-        -ms-transform: translateY(-50%);
-        transform: translateY(-50%);
-        -webkit-transition: left 0.1s cubic-bezier(0.4,1,0.75,0.9);
-        transition: left 0.1s cubic-bezier(0.4,1,0.75,0.9);
+        height: 12px;
+        border-radius: full;
+        -webkit-transform: translateX(0);
+        -ms-transform: translateX(0);
+        transform: translateX(0);
+        -webkit-transition: -webkit-transform 0.1s cubic-bezier(0.4,1,0.75,0.9);
+        -webkit-transition: transform 0.1s cubic-bezier(0.4,1,0.75,0.9);
+        transition: transform 0.1s cubic-bezier(0.4,1,0.75,0.9);
       }
 
       <label
-        className="emotion-2"
+        className="emotion-3"
       >
         <input
           className="emotion-0"
           type="checkbox"
         />
-        <span
-          className="emotion-1"
-        />
+        <div
+          className="emotion-2"
+        >
+          <div
+            className="emotion-1"
+          />
+        </div>
         Toggle this
       </label>
     `)
