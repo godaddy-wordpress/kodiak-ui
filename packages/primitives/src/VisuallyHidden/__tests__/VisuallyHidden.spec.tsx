38778ab82ee8bba6bef2cf256f9489b082ccf239
Wes Cole <wesley.cole87@gmail.com>
2020-02-11 12:26:39 -0500

    Add tests for VisuallyHidden

---
+++ packages/primitives/src/VisuallyHidden/__tests__/VisuallyHidden.spec.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import serializer from 'jest-emotion'
+import renderer from 'react-test-renderer'
+import { VisuallyHidden } from '../VisuallyHidden'
+
+expect.addSnapshotSerializer(serializer)
+
+describe('VisuallyHidden', () => {
+  it('should render the VisuallyHidden component with the appropriate styling to hide the text', () => {
+    expect(
+      renderer
+        .create(<VisuallyHidden>This text will be hidden</VisuallyHidden>)
+        .toJSON(),
+    ).toMatchInlineSnapshot(`
+      .emotion-0 {
+        border: 0;
+        -webkit-clip: rect(0 0 0 0);
+        clip: rect(0 0 0 0);
+        height: 1px;
+        margin: -1px;
+        overflow: hidden;
+        padding: 0;
+        position: absolute;
+        width: 1px;
+        white-space: nowrap;
+        word-wrap: normal;
+      }
+
+      <span
+        className="emotion-0"
+      >
+        This text will be hidden
+      </span>
+    `)
+  })
+})
