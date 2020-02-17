0407b68d7517d36b897db2f6ae18e0b5a5a7afb5
Wes Cole <wesley.cole87@gmail.com>
2020-02-17 14:50:04 -0500

    Add role and type and styled message container that accepts variant and system props

--- packages/message/src/Message.tsx
+++ packages/message/src/Message.tsx
@@ -1,4 +1,12 @@
 import * as React from 'react'
+import styled from '@emotion/styled'
+import {
+  systemProps,
+  SystemProps,
+  variant,
+  VariantProps,
+  sx,
+} from '@kodiak-ui/core'
 
 type MessageType = 'polite' | 'assertive'
 
@@ -6,8 +14,28 @@ type MessageProps = {
   children: React.ReactNode
   type?: MessageType
   onDismiss?: () => void
-}
+} & React.HTMLAttributes<HTMLDivElement> &
+  VariantProps &
+  SystemProps
 
-export function Message({ children, ...props }: MessageProps) {
-  return <div {...props}>{children}</div>
-}
+const MessageContainer = styled('div')(
+  ({ variant: variantProp, variantKey = 'messages', theme }) =>
+    variant({ variant: variantProp, theme, variantKey }),
+  ...systemProps,
+  sx,
+)
+
+export const Message = React.forwardRef<HTMLDivElement, MessageProps>(
+  function Message({ children, type = 'polite', ...props }, forwardedRef) {
+    return (
+      <MessageContainer
+        {...props}
+        ref={forwardedRef}
+        role={type === 'assertive' ? 'alert' : 'status'}
+        aria-live={type}
+      >
+        {children}
+      </MessageContainer>
+    )
+  },
+)
