9325f63ec40ab1212d17179dfdf4f75994eced95
Alex Chan <alexander.m.chan@gmail.com>
2020-02-19 21:29:02 -0700

    Find the closest theme color

--- packages/storybook/src/designsystem/Colors.stories.tsx
+++ packages/storybook/src/designsystem/Colors.stories.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { Text, Flex, Grid, Box } from '@kodiak-ui/primitives'
-import { useThemeUI } from 'theme-ui'
+import { useThemeUI, Theme } from 'theme-ui'
+import { useForm } from 'react-hook-form'
+import { parseToRgb, parseToHsl } from 'polished'
+import { RgbColor, RgbaColor } from 'polished/lib/types/color'
+
+import { Field, FieldError } from '@kodiak-ui/primitives'
 
 export default {
   title: 'Design System/Colors',
@@ -40,10 +45,19 @@ type ColorSwatchesProps = {
 } & React.ComponentProps<typeof Flex>
 
 function ColorSwatches({ colorsArray, colorName }: ColorSwatchesProps) {
-  // Would be nice to choose this by darkest color, maybe use d3.hsl to compare lightness
-  // since ideally this would support different string color formats
-  // or if convention is lightest to darkest we could take the last color
-  const borderLeftColor = colorsArray.find(color => !!color)
+  const borderLeftColor = colorsArray
+    .sort(function darkestColorForSort(color1, color2) {
+      try {
+        const color1Hsl = parseToHsl(color1)
+        const color2Hsl = parseToHsl(color2)
+
+        return color1Hsl.lightness - color2Hsl.lightness
+      } catch (error) {
+        return -1
+      }
+    })
+
+    .find(color => !!color)
 
   return (
     <Flex flexDirection="column" key={colorName} width="100%">
@@ -73,6 +87,170 @@ function ColorSwatches({ colorsArray, colorName }: ColorSwatchesProps) {
         })}
       </Grid>
     </Flex>
+  )
+}
+
+type VisitorFunction = ({
+  value,
+  key,
+  parentKeys,
+}: {
+  value?: any
+  key?: string
+  parentKeys?: string
+}) => void
+
+// lightweight deep map function
+function deepMapKeys({
+  obj,
+  visitorfunction,
+  key,
+  parentKeys,
+}: {
+  obj: any
+  visitorfunction: VisitorFunction
+  key?: string
+  parentKeys?: string
+}) {
+  if (typeof obj !== 'object') {
+    visitorfunction({ value: obj, key, parentKeys })
+  } else {
+    if (obj) {
+      Object.keys(obj).map(objectKey => {
+        deepMapKeys({
+          obj: obj[objectKey],
+          visitorfunction,
+          key: objectKey,
+          parentKeys:
+            parentKeys !== undefined ? [parentKeys, key].join('.') : key,
+        })
+      })
+    }
+  }
+}
+
+type ColorData = {
+  color: string
+}
+
+function flattenThemeColors({ theme }: { theme: Theme }) {
+  const flattenedThemeColors: Array<{
+    colorKey: string
+    color: string
+    rgbColor: RgbColor | RgbaColor
+  }> = []
+
+  deepMapKeys({
+    obj: theme.colors,
+    visitorfunction: ({ value, key, parentKeys }) => {
+      const colorKey =
+        parentKeys !== undefined ? [parentKeys, key].join('.') : key
+
+      if (!colorKey) {
+        return
+      }
+
+      try {
+        const rgbColor = parseToRgb(value)
+
+        flattenedThemeColors.push({
+          colorKey: colorKey,
+          color: value,
+          rgbColor: rgbColor,
+        })
+      } catch (error) {
+        console.error(`Couldn't parse ${colorKey}:${value}`)
+        console.error(error)
+      }
+    },
+  })
+
+  return flattenedThemeColors
+}
+
+function findNearestThemeColors({
+  theme,
+  color,
+  count,
+}: {
+  theme: Theme
+  color: string
+  count: number
+}) {
+  try {
+    const { red, green, blue } = parseToRgb(color)
+
+    const flattenedThemeColors = flattenThemeColors({ theme })
+    const colorsWithDistance = flattenedThemeColors
+      .map(color => ({
+        ...color,
+        distance: Math.sqrt(
+          2 * Math.pow(color.rgbColor.red - red, 2) +
+            4 * Math.pow(color.rgbColor.green - green, 2) +
+            3 * Math.pow(color.rgbColor.blue - blue, 2),
+        ),
+      }))
+      .sort((a, b) => a.distance - b.distance)
+
+    colorsWithDistance.length = count
+
+    return colorsWithDistance
+  } catch (e) {
+    // couldn't parse the color
+    return null
+  }
+}
+export function NearestThemeColor() {
+  const { register, watch } = useForm<ColorData>()
+  const { theme } = useThemeUI()
+  const color = watch('color')
+
+  if (theme.colors === undefined) {
+    return null
+  }
+
+  const nearestColors = findNearestThemeColors({ theme, color, count: 6 })
+
+  return (
+    <>
+      <form>
+        <Field
+          label="Find the nearest theme colors"
+          name="color"
+          ref={register({ required: false })}
+          aria-describedby="error-first-name-required"
+        ></Field>
+        {color && !nearestColors && (
+          <FieldError id="error-first-name-required">
+            Please enter a valid color (including # for hex)
+          </FieldError>
+        )}
+      </form>
+
+      {nearestColors && (
+        <Flex>
+          <Box>
+            <ColorSwatch
+              key={color}
+              color={color}
+              colorName={'Your color'}
+              pt={4}
+            />
+          </Box>
+
+          <Box>
+            {nearestColors.map((color, index) => (
+              <ColorSwatch
+                key={index}
+                color={color.color}
+                colorName={color.colorKey}
+                pt={4}
+              />
+            ))}
+          </Box>
+        </Flex>
+      )}
+    </>
   )
 }
 
