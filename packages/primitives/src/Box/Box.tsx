93e5def4e06dc8c174d7c4103a3f41db01b191da
Alex Chan <alexander.m.chan@gmail.com>
2020-02-24 13:25:16 -0700

    Export base

--- packages/primitives/src/Box/Box.tsx
+++ packages/primitives/src/Box/Box.tsx
@@ -74,6 +74,11 @@ export function sx(props: {
  * base function allow the __base property to set default values
  * that are read from the theme but can still be overridden by theme variants
  */
+
+export type BaseProps = {
+  __base?: SxStyleProp
+}
+
 export function base(props: { theme: Theme; __base?: SxStyleProp }) {
   return css(props.__base as any)(props.theme)
 }
