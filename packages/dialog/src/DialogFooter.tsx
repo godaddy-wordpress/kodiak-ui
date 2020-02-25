2b570ae0929759f30f843d4c13ff0f410d530435
Alex Chan <alexanderchan@users.noreply.github.com>
2020-02-25 14:08:26 -0700

    Fix the css prop for dialog (#37)

--- packages/dialog/src/DialogFooter.tsx
+++ packages/dialog/src/DialogFooter.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { InterpolationWithTheme } from '@emotion/core'
 import { css, Theme } from 'theme-ui'
 import { SerializedStyles } from '@emotion/serialize'
 import {
@@ -11,6 +12,7 @@ import {
 
 type DialogFooterProps = {
   children: React.ReactNode
+  css?: InterpolationWithTheme<any> // TODO: this type shouldn't be required
 } & SystemProps &
   VariantProps
 
