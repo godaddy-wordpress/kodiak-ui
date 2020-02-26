2e12bef952b038715f6d9327b1a157d333e4a886
Alex Chan <alexander.m.chan@gmail.com>
2020-02-26 10:51:13 -0700

    Update spec snapshots

--- packages/primitives/src/Menu/__tests__/Menu.spec.tsx
+++ packages/primitives/src/Menu/__tests__/Menu.spec.tsx
@@ -9,15 +9,22 @@ expect.addSnapshotSerializer(serializer)
 
 describe('Menu', () => {
   it('should render the Menu', () => {
-    expect(renderer.create(<Menu aria-label="Menu in test"></Menu>).toJSON())
-      .toMatchInlineSnapshot(`
-      .emotion-1 {
+    expect(
+      renderer
+        .create(
+          <Menu aria-label="Menu in test">
+            <MenuItem>Product</MenuItem>
+          </Menu>,
+        )
+        .toJSON(),
+    ).toMatchInlineSnapshot(`
+      .emotion-0 {
         box-sizing: border-box;
         margin: 0;
         min-width: 0;
       }
 
-      .emotion-0 {
+      .emotion-1 {
         box-sizing: border-box;
         margin: 0;
         min-width: 0;
@@ -27,12 +34,18 @@ describe('Menu', () => {
       }
 
       <nav
-        className="emotion-1"
+        className="emotion-0"
       >
         <ul
           aria-label="Menu in test"
-          className="emotion-0"
-        />
+          className="emotion-1"
+        >
+          <li
+            className="emotion-0"
+          >
+            Product
+          </li>
+        </ul>
       </nav>
     `)
   })
@@ -48,43 +61,37 @@ describe('Menu', () => {
         )
         .toJSON(),
     ).toMatchInlineSnapshot(`
-.emotion-1 {
-  box-sizing: border-box;
-  margin: 0;
-  min-width: 0;
-}
+      .emotion-1 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+      }
 
-.emotion-0 {
-  border: 0;
-  -webkit-clip: rect(0 0 0 0);
-  clip: rect(0 0 0 0);
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  width: 1px;
-  white-space: nowrap;
-  word-wrap: normal;
-}
+      .emotion-0 {
+        border: 0;
+        -webkit-clip: rect(0 0 0 0);
+        clip: rect(0 0 0 0);
+        height: 1px;
+        margin: -1px;
+        overflow: hidden;
+        padding: 0;
+        position: absolute;
+        width: 1px;
+        white-space: nowrap;
+        word-wrap: normal;
+      }
 
-<div
-  className="emotion-1"
->
-  <li>
-    <span
-      className="emotion-1"
-    >
       <span
-        className="emotion-0"
+        className="emotion-1"
       >
-        Current category: 
+        <span
+          className="emotion-0"
+        >
+          Current category: 
+        </span>
+        Product announcements
       </span>
-      Product announcements
-    </span>
-  </li>
-</div>
-`)
+    `)
   })
 
   it('should render the MenuItems', () => {
@@ -93,80 +100,51 @@ describe('Menu', () => {
         .create(
           <Menu aria-label="Menu with current">
             <MenuItem>Item 0</MenuItem>
-            <MenuItem isCurrent={true}>Item 1</MenuItem>
+            <MenuItem>Item 1</MenuItem>
             <MenuItem>Item 2</MenuItem>
           </Menu>,
         )
         .toJSON(),
     ).toMatchInlineSnapshot(`
-.emotion-1 {
-  box-sizing: border-box;
-  margin: 0;
-  min-width: 0;
-}
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+      }
 
-.emotion-6 {
-  box-sizing: border-box;
-  margin: 0;
-  min-width: 0;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
+      .emotion-3 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+      }
 
-.emotion-0 {
-  box-sizing: border-box;
-  margin: 0;
-  min-width: 0;
-  -webkit-text-decoration: none;
-  text-decoration: none;
-  color: inherit;
-  cursor: pointer;
-}
-
-<nav
-  className="emotion-1"
->
-  <ul
-    aria-label="Menu with current"
-    className="emotion-6"
-  >
-    <div
-      className="emotion-1"
-    >
-      <li>
-        <a
-          className="emotion-0"
+      <nav
+        className="emotion-0"
+      >
+        <ul
+          aria-label="Menu with current"
+          className="emotion-3"
         >
-          Item 0
-        </a>
-      </li>
-    </div>
-    <div
-      className="emotion-1"
-    >
-      <li>
-        <a
-          aria-current={true}
-          className="emotion-0"
-        >
-          Item 1
-        </a>
-      </li>
-    </div>
-    <div
-      className="emotion-1"
-    >
-      <li>
-        <a
-          className="emotion-0"
-        >
-          Item 2
-        </a>
-      </li>
-    </div>
-  </ul>
-</nav>
-`)
+          <li
+            className="emotion-0"
+          >
+            Item 0
+          </li>
+          <li
+            className="emotion-0"
+          >
+            Item 1
+          </li>
+          <li
+            className="emotion-0"
+          >
+            Item 2
+          </li>
+        </ul>
+      </nav>
+    `)
   })
 })
