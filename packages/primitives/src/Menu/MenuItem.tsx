74e960127cda4524d95ebc7ae40eee26d7fa1a65
Alex Chan <alexander.m.chan@gmail.com>
2020-02-26 09:21:21 -0700

    Simplify menu component

--- packages/primitives/src/Menu/MenuItem.tsx
+++ packages/primitives/src/Menu/MenuItem.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react'
 import { Box } from '../Box'
-import { Link } from '../Link'
 
 type MenuItemProps = {
   children: React.ReactNode
-  isCurrent?: boolean
-  href?: string
 } & React.ComponentProps<typeof Box>
 
 export const MenuItem = React.forwardRef<HTMLLIElement, MenuItemProps>(
   function MenuItem(
     {
       children,
-      isCurrent,
       variantKey = 'menuitem',
       variant,
       as: renderAs = 'li',
-      href,
       ...props
     },
     forwardedRef,
@@ -29,20 +24,7 @@ export const MenuItem = React.forwardRef<HTMLLIElement, MenuItemProps>(
         as={renderAs}
         ref={forwardedRef as any}
       >
-        {renderAs === 'a' && (
-          <Link
-            __base={{
-              textDecoration: 'none',
-              color: 'inherit',
-              cursor: 'pointer',
-            }}
-            href={href}
-            aria-current={isCurrent}
-          >
-            {children}
-          </Link>
-        )}
-        {renderAs !== 'a' && <Box as={renderAs}>{children}</Box>}
+        {children}
       </Box>
     )
   },
