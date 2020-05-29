a7a86034f3604e2cce223d8826f45b51e48acadd
seri <seritrinh@gmail.com>
2020-05-30 00:07:01 +0700

    Add snapshot test for the Image component

---
+++ packages/primitives/src/Image/__tests__/Image.spec.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import serializer from 'jest-emotion'
+import renderer from 'react-test-renderer'
+import { Image } from '../Image'
+
+expect.addSnapshotSerializer(serializer)
+
+describe('Image', () => {
+  it('should render the Image as an img element', () => {
+    expect(
+      renderer
+        .create(
+          <Image
+            src="image.jpg"
+            alt="T"
+            sx={{ width: '28px', height: '28px' }}
+          />,
+        )
+        .toJSON(),
+    ).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        width: 28px;
+        height: 28px;
+      }
+
+      <img
+        alt="T"
+        className="emotion-0"
+        src="image.jpg"
+      />
+    `)
+  })
+})
