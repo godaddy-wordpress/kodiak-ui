539190689cb74894982494a1eaa97528f6b93a42
Alex Chan <alexanderchan@users.noreply.github.com>
2020-05-29 19:16:42 -0600

    Add loading skeleton package (#70)
    * Add skeleton package
    
    * Storybook setup
    
    * Add comments
    
    * Import from core to fix bundle size
    
    * Update snapshot tests
    
    * Fix animation for Skeleton
    
    * Update package dependencies
    
    Co-authored-by: Wes Cole <wesley.cole87@gmail.com>

---
+++ packages/skeleton/src/Skeleton.tsx
@@ -0,0 +1,84 @@
+/** @jsx jsx */
+import { css, keyframes, jsx } from '@emotion/core'
+import * as React from 'react'
+import { Box } from '@kodiak-ui/primitives'
+
+type SkeletonProps = { duration?: number } & React.ComponentProps<typeof Box>
+
+// based on https://github.com/dvtng/react-loading-skeleton
+
+export const skeletonKeyframes = keyframes`
+  0% {
+    background-position: -200px 0;
+  }
+  100% {
+    background-position: calc(200px + 100%) 0;
+  }
+`
+
+const skeletonColor = '#eee'
+const defaultHighlightColor = '#f5f5f5'
+
+const skeletonBaseStyles = {
+  backgroundColor: skeletonColor,
+  backgroundImage: `linear-gradient(
+      90deg,
+      ${skeletonColor},
+      ${defaultHighlightColor},
+      ${skeletonColor}
+    )`,
+  backgroundSize: `200px 100%`,
+  backgroundRepeat: `no-repeat`,
+  borderRadius: 'default',
+  display: 'inline-block',
+  lineHeight: 1,
+  width: '100%',
+}
+
+type RepeatProps = { count?: number; children: React.ReactNode }
+
+export function Repeat({
+  count = 1,
+  children,
+}: RepeatProps): React.ReactElement {
+  const lines = new Array(count).fill(null)
+
+  return (
+    <React.Fragment>
+      {lines.map((_, index) => {
+        return <React.Fragment key={index}>{children}</React.Fragment>
+      })}
+    </React.Fragment>
+  )
+}
+
+export const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
+  function Skeleton(
+    {
+      variantKey = 'skeletons',
+      variant,
+      duration = '1.2s',
+      children,
+      ...props
+    },
+    forwardedRef,
+  ) {
+    return (
+      <Box as="span">
+        <Box
+          __base={{
+            ...skeletonBaseStyles,
+            animation: `${skeletonKeyframes} ${duration} ease-in-out infinite`,
+          }}
+          ref={forwardedRef as any}
+          variantKey={variantKey}
+          variant={variant}
+          as="span"
+          {...props}
+        >
+          &zwnj;{children}
+        </Box>
+      </Box>
+    )
+  },
+)
---
+++ packages/skeleton/src/__tests__/Skeleton.spec.tsx
@@ -0,0 +1,151 @@
+import * as React from 'react'
+import serializer from 'jest-emotion'
+import renderer from 'react-test-renderer'
+import { Skeleton, Repeat } from '../Skeleton'
+
+expect.addSnapshotSerializer(serializer)
+
+describe('Accordion', () => {
+  it('should render the Skeleton', () => {
+    expect(renderer.create(<Skeleton />).toJSON()).toMatchInlineSnapshot(`
+      @keyframes animation-0 {
+        0% {
+          background-position: -200px 0;
+        }
+
+        100% {
+          background-position: calc(200px + 100%) 0;
+        }
+      }
+
+      .emotion-1 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+      }
+
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        background-color: #eee;
+        background-image: linear-gradient( 90deg, #eee, #f5f5f5, #eee );
+        background-size: 200px 100%;
+        background-repeat: no-repeat;
+        border-radius: default;
+        display: inline-block;
+        line-height: 1;
+        width: 100%;
+        -webkit-animation: animation-0 1.2s ease-in-out infinite;
+        animation: animation-0 1.2s ease-in-out infinite;
+      }
+
+      <span
+        className="emotion-1"
+      >
+        <span
+          className="emotion-0"
+        >
+          ‌
+        </span>
+      </span>
+    `)
+  })
+
+  it('should render multiple skeletons', () => {
+    expect(
+      renderer
+        .create(
+          <Repeat count={2}>
+            <Skeleton />
+          </Repeat>,
+        )
+        .toJSON(),
+    ).toMatchInlineSnapshot(`
+      Array [
+        @keyframes animation-0 {
+        0% {
+          background-position: -200px 0;
+        }
+
+        100% {
+          background-position: calc(200px + 100%) 0;
+        }
+      }
+
+      .emotion-1 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+      }
+
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        background-color: #eee;
+        background-image: linear-gradient( 90deg, #eee, #f5f5f5, #eee );
+        background-size: 200px 100%;
+        background-repeat: no-repeat;
+        border-radius: default;
+        display: inline-block;
+        line-height: 1;
+        width: 100%;
+        -webkit-animation: animation-0 1.2s ease-in-out infinite;
+        animation: animation-0 1.2s ease-in-out infinite;
+      }
+
+      <span
+          className="emotion-1"
+        >
+          <span
+            className="emotion-0"
+          >
+            ‌
+          </span>
+        </span>,
+        @keyframes animation-0 {
+        0% {
+          background-position: -200px 0;
+        }
+
+        100% {
+          background-position: calc(200px + 100%) 0;
+        }
+      }
+
+      .emotion-1 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+      }
+
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        background-color: #eee;
+        background-image: linear-gradient( 90deg, #eee, #f5f5f5, #eee );
+        background-size: 200px 100%;
+        background-repeat: no-repeat;
+        border-radius: default;
+        display: inline-block;
+        line-height: 1;
+        width: 100%;
+        -webkit-animation: animation-0 1.2s ease-in-out infinite;
+        animation: animation-0 1.2s ease-in-out infinite;
+      }
+
+      <span
+          className="emotion-1"
+        >
+          <span
+            className="emotion-0"
+          >
+            ‌
+          </span>
+        </span>,
+      ]
+    `)
+  })
+})
