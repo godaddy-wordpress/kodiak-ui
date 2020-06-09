a727309bae5e1928037ba7adcb77155d4da0005d
Wes Cole <wesley.cole87@gmail.com>
2020-06-08 22:26:50 -0400

    Generate tabs for rendering

--- packages/tabs/src/useTabs.ts
+++ packages/tabs/src/useTabs.ts
@@ -1,20 +1,46 @@
 import * as React from 'react'
 
+interface UserTab {
+  tab: string | React.ReactNode
+  panel: string | React.ReactNode
+}
+
+type Tab = {
+  children: React.ReactNode
+} & React.HTMLAttributes<HTMLButtonElement>
+
 interface UseTabsOptions {
   initialIndex?: number
-  tabs: { tab: string | React.ReactNode; panel: string | React.ReactNode }[]
+  tabs: UserTab[]
 }
 
 interface UseTabsReturn {
-  selectedIndex: number
+  tabs: any[]
+  tabPanels: any[]
 }
 
 export function useTabs(
-  { initialIndex = 0, tabs }: UseTabsOptions = { tabs: [] },
+  { initialIndex = 0, tabs: userTabs }: UseTabsOptions = { tabs: [] },
 ): UseTabsReturn {
   const [selectedIndex, setSelectedIndex] = React.useState(initialIndex)
 
+  const tabs = React.useMemo(
+    function generateTabs(): Tab[] {
+      return userTabs?.map(({ tab }: UserTab, index: number) => ({
+        children: tab,
+        tabIndex: -1,
+        'aria-selected': selectedIndex === index,
+      }))
+    },
+    [userTabs, selectedIndex],
+  )
+
+  const tabPanels = React.useMemo(function generateTabPanels() {
+    return []
+  }, [])
+
   return {
-    selectedIndex,
+    tabs,
+    tabPanels,
   }
 }
