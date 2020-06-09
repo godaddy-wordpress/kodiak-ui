3ef0f93eba3b8d95d510571446b259b0c7ef209e
Wes Cole <wesley.cole87@gmail.com>
2020-06-09 15:11:28 -0400

    Work on keyboard navigation

--- packages/tabs/src/useTabs.ts
+++ packages/tabs/src/useTabs.ts
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { SxStyleProp } from 'theme-ui'
+import { getNextIndex } from '@kodiak-ui/utils'
 import { useId } from '@kodiak-ui/hooks'
 
 interface UserTab {
@@ -21,55 +22,132 @@ interface UseTabsOptions {
 }
 
 interface UseTabsReturn {
-  selectedIndex: number
+  selectedIndex: number | string
   tabs: Tab[]
   tabPanels: TabPanel[]
+}
+
+interface RegisterOptions {
+  name: string
+}
+
+interface RefAndOptions<Element> {
+  ref: Element | null
+  options: RegisterOptions | null
 }
 
 export function useTabs(
   { initialIndex = 0, tabs: userTabs }: UseTabsOptions = { tabs: [] },
 ): UseTabsReturn {
   const id = useId()
-  const [selectedIndex, setSelectedIndex] = React.useState(initialIndex)
+  const tabsRef = React.useRef<{ [key: string]: Element | null }>({})
+  const [selectedIndex, setSelectedIndex] = React.useState<string | number>(
+    initialIndex,
+  )
 
-  const handleOnClick = React.useCallback(function handleOnClick(
-    index: number,
-  ) {
-    setSelectedIndex(index)
-  },
-  [])
+  const onClick = React.useCallback(function handleOnClick(index: number) {
+    return () => setSelectedIndex(index)
+  }, [])
+
+  function getItemNodeFromIndex(index: number): Element | null {
+    return tabsRef && tabsRef.current && tabsRef.current[index]
+  }
 
   const onKeyUp = React.useCallback(function handleOnKeyUp(
     event: React.KeyboardEvent,
   ) {
+    console.log(tabsRef.current)
     switch (event.key) {
       case 'ArrowLeft':
-        console.log('ArrowLeft')
+        const nextIndex = getNextIndex({
+          moveAmount: -1,
+          baseIndex: Object.keys(tabsRef.current).indexOf(
+            selectedIndex as string,
+          ),
+          items: tabsRef.current,
+          getItemNodeFromIndex,
+        })
+        console.log(nextIndex)
+        setSelectedIndex(nextIndex)
         break
       case 'ArrowRight':
         console.log('ArrowRight')
         break
+      case 'Enter':
+        console.log('Enter')
+        break
+      case 'Home':
+        console.log('Home')
+        break
+      case 'End':
+        console.log('End')
+        break
     }
   },
   [])
+
+  function registerTabElements<TElement extends HTMLButtonElement>({
+    ref,
+    options,
+  }: RefAndOptions<TElement>): RefAndOptions<TElement> {
+    const name = options && options.name
+    const tabs = tabsRef && tabsRef.current
+
+    tabs[name as string] = ref
+
+    return { ref, options }
+  }
+
+  function registerElementRefs<TElement extends HTMLButtonElement>(
+    ref: TElement | null,
+    options: RegisterOptions | null = null,
+  ): RefAndOptions<TElement> {
+    return registerTabElements({ ref, options })
+  }
+
+  function register<TElement extends HTMLButtonElement>(): (
+    ref: TElement | null,
+  ) => void
+  function register<TElement extends HTMLButtonElement>(
+    options: RegisterOptions,
+  ): (ref: TElement | null) => void
+  function register<TElement extends HTMLButtonElement>(
+    ref: TElement | null,
+    options?: RegisterOptions,
+  ): void
+  function register<TElement extends HTMLButtonElement>(
+    refOrOptions?: RegisterOptions | TElement | null,
+    options?: RegisterOptions,
+  ): ((ref: TElement | null) => void) | void {
+    if (refOrOptions instanceof Element) {
+      registerElementRefs(refOrOptions as TElement | null, options)
+    }
+
+    return function (ref: TElement | null) {
+      ref && registerElementRefs(ref, refOrOptions as RegisterOptions)
+    }
+  }
+
+  const registerCallback = React.useCallback(register, [])
 
   const tabs = React.useMemo(
     function generateTabProps(): Tab[] {
       return (
         userTabs &&
         userTabs.map(({ tab }: UserTab, index: number) => ({
+          ref: registerCallback({ name: `tabitem-${index}` }),
           id: `kodiak-ui-tabs-${id}-tabitem-${index}`,
           children: tab,
           tabIndex: selectedIndex === index ? 0 : -1,
           role: 'tab',
-          onClick: () => handleOnClick(index),
+          onClick: onClick(index),
           onKeyUp,
           'aria-controls': `kodiak-ui-tabs-${id}-tabpanel-${index}`,
           'aria-selected': selectedIndex === index,
         }))
       )
     },
-    [id, userTabs, selectedIndex],
+    [id, userTabs, selectedIndex, registerCallback, onClick, onKeyUp],
   )
 
   const tabPanels = React.useMemo(
