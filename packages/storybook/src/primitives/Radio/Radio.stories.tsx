7eab0a426bec1694be64f0e6309356f147659f25
Wesley Cole <wesley.cole87@gmail.com>
2020-06-11 14:06:23 -0400

    Refactor the Radio input in Kodiak UI to utilize sx and remove styled system (#74)
    * Refactor Radio to set sx properly and remove system props
    
    * Update types
    
    * Update snapshots

--- packages/storybook/src/primitives/Radio/Radio.stories.tsx
+++ packages/storybook/src/primitives/Radio/Radio.stories.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import { Label, Radio } from '@kodiak-ui/primitives'
+import { Box, Flex, Text, Label, Radio } from '@kodiak-ui/primitives'
 
 import { withA11y } from '@storybook/addon-a11y'
 
 export default { title: 'Forms/Radio', decorators: [withA11y] }
 
-export function initial() {
+export function Initial() {
   return (
     <>
       <Label display="flex" alignItems="center">
@@ -20,6 +20,42 @@ export function initial() {
       <Label display="flex" alignItems="center">
         <Radio name="dark-mode" value="false" />
         Light Mode
+      </Label>
+    </>
+  )
+}
+
+export function Multiline() {
+  return (
+    <>
+      <Label>
+        <Flex sx={{ alignItems: 'center' }}>
+          <Radio
+            variant="radio"
+            name="dark-mode"
+            value="true"
+            defaultChecked={true}
+          />
+          Dark Mode
+        </Flex>
+        <Box>
+          <Text sx={{ fontWeight: 'normal' }}>
+            This is some more text that we want to render in the label so that
+            it is clickable
+          </Text>
+        </Box>
+      </Label>
+      <Label>
+        <Flex sx={{ alignItems: 'center' }}>
+          <Radio name="dark-mode" value="false" />
+          Light Mode
+        </Flex>
+        <Box>
+          <Text sx={{ fontWeight: 'normal' }}>
+            This is some more text that we want to render in the label so that
+            it is clickable
+          </Text>
+        </Box>
       </Label>
     </>
   )
