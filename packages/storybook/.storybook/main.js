a60a1f18a9e3a33da2d9ccfc733b8a3caad446e5
Alex Chan <alex@skyverge.com>
2020-07-13 14:04:14 -0600

    Storybook working but slow build

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,5 +1,21 @@
 module.exports = {
   stories: ['../src/**/*.stories.(js|ts|tsx|mdx)'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
   addons: [
     '@storybook/preset-typescript',
     '@storybook/addon-a11y/register',
