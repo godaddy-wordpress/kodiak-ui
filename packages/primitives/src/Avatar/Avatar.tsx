0784cc97343139fb86bd7be7e1950db01c174b28
Alex Chan <alex@skyverge.com>
2020-08-06 13:01:44 -0600

    Fix type

--- packages/primitives/src/Avatar/Avatar.tsx
+++ packages/primitives/src/Avatar/Avatar.tsx
@@ -66,75 +66,77 @@ function useLoadImage({
   return { imageState }
 }
 
-export const Avatar = React.forwardRef<HTMLDivElement>(function Avatar(
-  {
-    children: childrenProp,
-    src,
-    srcSet,
-    alt,
-    sizes,
-    imgProps,
-    ...props
-  }: AvatarProps,
-  ref,
-) {
-  const { imageState } = useLoadImage({ src, srcSet })
-  const hasImg = src || srcSet
-  const hasImgNotFailing = hasImg && imageState !== ImageState.error
+export const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(
+  function Avatar(
+    {
+      children: childrenProp,
+      src,
+      srcSet,
+      alt,
+      sizes,
+      imgProps,
+      ...props
+    }: AvatarProps,
+    ref,
+  ) {
+    const { imageState } = useLoadImage({ src, srcSet })
+    const hasImg = src || srcSet
+    const hasImgNotFailing = hasImg && imageState !== ImageState.error
 
-  let children = null
+    let children = null
 
-  if (hasImgNotFailing) {
-    children = (
-      <KodiakImage
-        __base={{
-          width: '100%',
-          height: '100%',
-          textAlign: 'center',
-          // Handle non-square image. The property isn't supported by IE 11.
-          objectFit: 'cover',
-          // Hide alt text.
-          color: 'transparent',
-          // Hide the image broken icon, only works on Chrome.
-          textIndent: 10000,
-        }}
+    if (hasImgNotFailing) {
+      children = (
+        <KodiakImage
+          __base={{
+            width: '100%',
+            height: '100%',
+            textAlign: 'center',
+            // Handle non-square image. The property isn't supported by IE 11.
+            objectFit: 'cover',
+            // Hide alt text.
+            color: 'transparent',
+            // Hide the image broken icon, only works on Chrome.
+            textIndent: 10000,
+          }}
+          variantKey="avatar"
+          variant="image"
+          alt={alt}
+          src={src}
+          srcSet={srcSet}
+          sizes={sizes}
+          {...imgProps}
+        />
+      )
+    } else if (childrenProp != null) {
+      children = childrenProp
+    } else if (hasImg && alt) {
+      children = alt[0]
+    } else {
+      children = null
+    }
+
+    return (
+      <Box
+        ref={ref}
         variantKey="avatar"
-        variant="image"
-        alt={alt}
-        src={src}
-        srcSet={srcSet}
-        sizes={sizes}
-        {...imgProps}
-      />
+        __base={{
+          alignItems: 'center',
+          borderRadius: 'full',
+          display: 'inline-flex',
+          flexShrink: 0,
+          justifyContent: 'center',
+          lineHeight: 1,
+          overflow: 'hidden',
+          position: 'relative',
+          userSelect: 'none',
+          width: 32,
+          height: 32,
+        }}
+        {...props}
+      >
+        {children}
+      </Box>
     )
-  } else if (childrenProp != null) {
-    children = childrenProp
-  } else if (hasImg && alt) {
-    children = alt[0]
-  } else {
-    children = null
-  }
-
-  return (
-    <Box
-      ref={ref}
-      variantKey="avatar"
-      __base={{
-        alignItems: 'center',
-        borderRadius: 'full',
-        display: 'inline-flex',
-        flexShrink: 0,
-        justifyContent: 'center',
-        lineHeight: 1,
-        overflow: 'hidden',
-        position: 'relative',
-        userSelect: 'none',
-        width: 32,
-        height: 32,
-      }}
-      {...props}
-    >
-      {children}
-    </Box>
-  )
-})
+  },
+)
