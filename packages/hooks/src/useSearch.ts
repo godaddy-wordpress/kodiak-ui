6f8c591de26b91b65768e31e83bc98a6a4cf3a57
Wes Cole <wesley.cole87@gmail.com>
2020-08-21 00:55:52 -0400

    Add useDebounce and useSearch

---
+++ packages/hooks/src/useSearch.ts
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { useDebounce } from './useDebounce'
+
+type SearchTerm = string | null
+
+interface UseSearchProps {
+  handler: (searchTerm: SearchTerm) => void
+}
+
+export function useSearch({
+  handler,
+}: UseSearchProps): [SearchTerm, React.Dispatch<React.SetStateAction<string>>] {
+  const [searchTerm, setSearchTerm] = React.useState<string>(null)
+  const debouncedSearchTerm = useDebounce<string>(searchTerm, 300)
+
+  React.useEffect(
+    function handleSearch() {
+      if (debouncedSearchTerm) {
+        handler?.(debouncedSearchTerm)
+      }
+    },
+    [debouncedSearchTerm, handler],
+  )
+
+  return [searchTerm, setSearchTerm]
+}
