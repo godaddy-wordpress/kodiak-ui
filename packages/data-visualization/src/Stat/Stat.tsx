567d70fca14de0d392089e54f1e003c0cde42341
Wes Cole <wesley.cole87@gmail.com>
2020-09-09 01:35:20 -0400

    Update primitive imports for Stat

--- packages/data-visualization/src/Stat/Stat.tsx
+++ packages/data-visualization/src/Stat/Stat.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import { SxStyleProp } from '@kodiak-ui/core'
-import { Box } from '@kodiak-ui/primitives/box'
-import { Flex } from '@kodiak-ui/primitives/flex'
-import { Grid } from '@kodiak-ui/primitives/grid'
-import { Label } from '@kodiak-ui/primitives/label'
+import Box from '@kodiak-ui/primitives/box'
+import Flex from '@kodiak-ui/primitives/flex'
+import Grid from '@kodiak-ui/primitives/grid'
+import Label from '@kodiak-ui/primitives/label'
 
 type Props = {
   label?: React.ReactNode
