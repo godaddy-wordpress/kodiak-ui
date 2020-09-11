a8c48d7882ea95b19185bdc238fcab1a6c803afc
Wes Cole <wesley.cole87@gmail.com>
2020-09-11 00:11:47 -0400

    Work on the theme

--- packages/kodiak-ui/src/index.ts
+++ packages/kodiak-ui/src/index.ts
@@ -1,21 +1,73 @@
+import * as CSS from 'csstype'
+import { ColorMode, Theme } from 'theme-ui'
 import themeDefault from './theme-default'
 
+export type ScaleArray<T> = T[]
+export type ScaleObject<T> = { [K: string]: T | Scale<T>; [I: number]: T }
+export type Scale<T> = ScaleArray<T> | ScaleObject<T>
+export type TLength = string | 0 | number
+
+export type ScaleColorMode = ColorMode & {
+  /**
+   * Nested color modes can provide overrides when used in conjunction with
+   * `Theme.initialColorModeName and `useColorMode()`
+   */
+  modes?: {
+    [k: string]: ColorMode
+  }
+}
+
+/**
+ * Kodiak UI enforces certain scale types for specific system types.
+ * While technically any scale will accept arrays or objects, we feel like
+ * there are cases where providing both options are confusing and not helpful to
+ * the developer.
+ *
+ */
+export type System = {
+  borders?: Scale<CSS.Property.Border<string>>
+  borderStyles?: Scale<CSS.Property.Border<string>>
+  borderWidths?: Scale<CSS.Property.BorderWidth<TLength>>
+  breakpoints?: ScaleArray<CSS.Property.Width<string>>
+  colors?: ScaleColorMode
+  fonts?: ScaleObject<CSS.Property.FontFamily>
+  fontSizes?: Scale<CSS.Property.FontSize<number>>
+  fontWeights?: ScaleObject<CSS.Property.FontWeight>
+  letterSpacings?: ScaleObject<CSS.Property.LetterSpacing<TLength>>
+  lineHeights?: ScaleObject<CSS.Property.LineHeight<TLength>>
+  mediaQueries?: { [size: string]: string }
+  radii?: ScaleObject<CSS.Property.BorderRadius<TLength>>
+  space?: Scale<CSS.Property.Margin<number | string>>
+  sizes?: ScaleObject<CSS.Property.Height<string> | CSS.Property.Width<string>>
+  shadows?: ScaleObject<CSS.Property.BoxShadow>
+  transitions?: ScaleObject<CSS.Property.Transition>
+  zIndices?: ScaleObject<CSS.Property.ZIndex>
+  // colorStyles?: Scale<ThemeUICSSProperties>
+  // textStyles?: Scale<ThemeUICSSProperties>
+  // opacities?: Scale<CSS.OpacityProperty>
+}
+
+export type Root = {
+  html: any
+  body: any
+}
+
+export type Theme = System & { styles?: any }
+
 type CreateDesignSystemOptions = {
-  breakpoints: any
-  space: any
+  system: System
+  root: Root
 }
 
-type CreateDesignSystemReturn = {
-  theme: any
-}
-
-export function createDesignSystem(
-  options: CreateDesignSystemOptions,
-): CreateDesignSystemReturn {
+export function createDesignSystem({
+  system,
+  root,
+}: CreateDesignSystemOptions): { theme: Theme } {
   const theme = {
     ...themeDefault,
-    ...options,
+    ...system,
   }
+
   return {
     theme,
   }
--- packages/kodiak-ui/src/theme-default.ts
+++ packages/kodiak-ui/src/theme-default.ts
@@ -1,6 +1,38 @@
-export default {
+import { System } from './'
+
+const breakpoints = ['40em', '56em', '64em']
+
+export const theme: System = {
+  breakpoints,
   colors: {
-    black: ['#000'],
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#30c',
+    muted: '#f6f6f6',
+  },
+  fonts: {
+    body:
+      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'inherit',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  mediaQueries: {
+    small: `@media screen and (min-width: ${breakpoints[0]})`,
+    medium: `@media screen and (min-width: ${breakpoints[1]})`,
+    large: `@media screen and (min-width: ${breakpoints[2]})`,
   },
   space: [0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80],
 }
+
+export default theme
