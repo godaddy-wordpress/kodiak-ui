94bc8f607087e8438706c6dc2a5ae04457f67add
Wes Cole <wesley.cole87@gmail.com>
2020-09-11 22:36:43 -0400

    Add clamp for fluid typography

--- packages/kodiak-ui/src/theme-default.ts
+++ packages/kodiak-ui/src/theme-default.ts
@@ -1,4 +1,4 @@
-import { Theme } from './'
+import { Theme } from 'theme-ui'
 
 const breakpoints = ['40em', '56em', '64em']
 
@@ -17,15 +17,24 @@ export const theme: Theme & { global: any } = {
     heading: 'inherit',
     monospace: 'Menlo, monospace',
   },
-  fontSizes: ['0', '1rem', '1.1rem', '1.2rem', '1.5rem', '2rem', '3rem'],
+  fontSizes: [
+    '0.75rem', // 0 => 12px
+    '0.875rem', // 1 => 14px
+    '1rem', // 2 => 16px
+    '1.25rem', // 3 => 20px
+    '1.5rem', //  4 => 24px
+    '2rem', // 5 => 32px
+    '2.5rem', // 6 => 40px
+    '3rem', // 7 => 48px
+  ],
   fontWeights: {
     body: 400,
     heading: 700,
     bold: 700,
   },
   lineHeights: {
-    body: 1.5,
-    heading: 1.125,
+    body: 1.25,
+    heading: 1.5,
   },
   mediaQueries: {
     small: `@media screen and (min-width: ${breakpoints[0]})`,
@@ -33,6 +42,14 @@ export const theme: Theme & { global: any } = {
     large: `@media screen and (min-width: ${breakpoints[2]})`,
   },
   space: [0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80],
+  text: {
+    heading: {
+      fontFamily: 'heading',
+      fontWeight: 'heading',
+      lineHeight: 'heading',
+      m: 0,
+    },
+  },
   global: {
     html: {
       fontFamily: 'body',
@@ -45,11 +62,12 @@ export const theme: Theme & { global: any } = {
       padding: 0,
     },
     h1: {
-      fontSize: [
-        '2.25rem',
-        'calc(2.25rem + (((100vw - 20rem) / (90 - 20))) * (4.75 - 2.25))',
-        '4.75rem',
-      ],
+      variant: 'text.heading',
+      fontSize: 'clamp(1.5rem, 1.5rem + 2vw, 2rem)', // min of 20px on small screens and max of 32 px on large screens
+    },
+    h2: {
+      variant: 'text.heading',
+      fontSize: 'clamp(1.25rem, 1.25rem + 2vw, 1.5rem)',
     },
   },
 }
