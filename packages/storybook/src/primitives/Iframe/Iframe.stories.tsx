dd89e007ffeacdf8a9c2925a591fa7aeb0f36ccd
Wes Cole <wesley.cole87@gmail.com>
2020-10-06 21:34:13 -0400

    Add variants support to Grid and Iframe

--- packages/storybook/src/primitives/Iframe/Iframe.stories.tsx
+++ packages/storybook/src/primitives/Iframe/Iframe.stories.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react'
+import { variant } from 'kodiak-ui'
 import { Iframe } from '@kodiak-ui/primitives'
 
-export default { title: 'Iframe', component: Iframe }
+export default { title: 'Primitives/Iframe', component: Iframe }
+
+variant('square', {
+  height: '600px',
+  width: '600px',
+})
 
 export function initial() {
   return (
     <Iframe
       title="Example iframe"
       src="http://jilt.com"
+      variants="square"
       sx={{
         boxShadow: 'none',
         border: '1px solid',
         borderColor: 'black',
-        height: '600px',
-        width: '100%',
       }}
     />
   )
