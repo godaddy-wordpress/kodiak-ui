662dfa1966c3ec6f519eaf100ba1464520cc6e6e
TAY TS <taytzushieh@gmail.com>
2020-10-13 02:30:02 +0800

    Add snapshot test to MenuList (#121)

---
+++ packages/menu/src/__tests__/MenuList.test.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import serializer from 'jest-emotion'
+import renderer from 'react-test-renderer'
+import { MenuList } from '../MenuList'
+
+expect.addSnapshotSerializer(serializer)
+
+describe('MenuList', () => {
+  it('should render MenuList', () => {
+    const component = renderer
+      .create(
+        <MenuList>
+          <div />
+        </MenuList>,
+      )
+      .toJSON()
+
+    expect(component).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        background-color: white;
+        border: 1px solid;
+        border-color: muted;
+        border-radius: default;
+        list-style: none;
+        margin: 0;
+        margin-top: 8px;
+        outline: none;
+        overflow: hidden;
+        padding: 0;
+      }
+
+      <ul
+        className="emotion-0"
+      >
+        <div />
+      </ul>
+    `)
+  })
+})
