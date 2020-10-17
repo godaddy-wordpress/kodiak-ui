d112af653fb55c6604ae463be2340417864ff553
Lukas Slivko <lukasslivko@gmail.com>
2020-10-18 00:19:49 +0200

    deleted d.ts and edited use-number-formatter

--- packages/hooks/src/__tests__/useNumberFormatter.spec.ts
+++ packages/hooks/src/__tests__/useNumberFormatter.spec.ts
@@ -11,6 +11,21 @@ describe('useNumberFormatter', () => {
       }));
       expect(format.result.current(134)).toBe("$134.00");
 
-      
+      const format2 = renderHook(() => useNumberFormatter("de-DE", {
+        style: "currency",
+        currency: "EUR",
+      }));
+      expect(format2.result.current(225.25)).toBe("225,25\xa0€");
+
+      const format3 = renderHook(() => useNumberFormatter("ja-JP", {
+        style: "currency",
+        currency: "JPY",
+      }));
+      expect(format3.result.current(5832.0)).toBe("￥5,832");
+
+      const format4 = renderHook(() => useNumberFormatter("en-IN", {
+        maximumSignificantDigits: 3
+      }));
+      expect(format4.result.current(123456.789)).toBe("1,23,000");
     })
   })
