eea1c3947902258ab132244e21eb86a2551d76d4
Alex Chan <alex@skyverge.com>
2020-10-21 15:38:43 -0600

    yarn run lint --fix

--- packages/hooks/src/useNumberFormatter.ts
+++ packages/hooks/src/useNumberFormatter.ts
@@ -1,17 +1,17 @@
-import React from 'react';
+import * as React from 'react'
 
 export default function useNumberFormatter(
-    locale?: string,
-    options?: Intl.NumberFormatOptions
-  ) {
-    const optionsRef = React.useRef(options);
-    optionsRef.current = options;
-    const numberFormatter = React.useMemo(() => {
-      return function formatNumber(number: number) {
-        const numberFormat = new Intl.NumberFormat(locale, optionsRef.current);
-        return numberFormat.format(number);
-      };
-    }, [locale]);
-  
-    return numberFormatter;
-  }
+  locale?: string,
+  options?: Intl.NumberFormatOptions,
+) {
+  const optionsRef = React.useRef(options)
+  optionsRef.current = options
+  const numberFormatter = React.useMemo(() => {
+    return function formatNumber(number: number) {
+      const numberFormat = new Intl.NumberFormat(locale, optionsRef.current)
+      return numberFormat.format(number)
+    }
+  }, [locale])
+
+  return numberFormatter
+}
