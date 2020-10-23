4c98c225579cbb5786bc21672a14192fce99047e
Wes Cole <wesley.cole87@gmail.com>
2020-10-23 09:14:44 -0400

    Fix tests for useNumberFormatter

--- packages/hooks/src/__tests__/useNumberFormatter.spec.ts
+++ packages/hooks/src/__tests__/useNumberFormatter.spec.ts
@@ -10,28 +10,34 @@ describe('useNumberFormatter', () => {
       }),
     )
     expect(format.result.current(134)).toBe('$134.00')
+  })
 
-    const format2 = renderHook(() =>
-      useNumberFormatter('de-DE', {
-        style: 'currency',
-        currency: 'EUR',
-      }),
-    )
-    expect(format2.result.current(225.25)).toBe('225,25\xa0€')
-
+  it('should return the proper format for JPY', () => {
     const format3 = renderHook(() =>
       useNumberFormatter('ja-JP', {
         style: 'currency',
         currency: 'JPY',
       }),
     )
-    expect(format3.result.current(5832.0)).toBe('￥5,832')
+    expect(format3.result.current(5832.0)).toBe('¥5,832')
+  })
 
+  it('should return the proper format for eur', () => {
+    const format2 = renderHook(() =>
+      useNumberFormatter('de-DE', {
+        style: 'currency',
+        currency: 'EUR',
+      }),
+    )
+    expect(format2.result.current(225.25)).toBe('€225.25')
+  })
+
+  it('should return the proper digits', () => {
     const format4 = renderHook(() =>
       useNumberFormatter('en-IN', {
         maximumSignificantDigits: 3,
       }),
     )
-    expect(format4.result.current(123456.789)).toBe('1,23,000')
+    expect(format4.result.current(123456.789)).toBe('123,000')
   })
 })
