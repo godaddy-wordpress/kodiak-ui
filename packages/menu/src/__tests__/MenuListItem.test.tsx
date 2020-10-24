65e603782ad2105ec8ce54739a0caa16430cf296
TAY TS <taytzushieh@gmail.com>
2020-10-24 23:35:38 +0800

    Add snapshot test to MenuListItem (#133)

---
+++ packages/menu/src/__tests__/MenuListItem.test.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react'
+import serializer from 'jest-emotion'
+import renderer from 'react-test-renderer'
+import { MenuListItem } from '../MenuListItem'
+
+expect.addSnapshotSerializer(serializer)
+
+describe('MenuListItem', () => {
+  it('should render MenuListItem with string as children', () => {
+    const component = renderer
+      .create(<MenuListItem>Item</MenuListItem>)
+      .toJSON()
+
+    expect(component).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        cursor: pointer;
+        line-height: 1;
+        padding-top: 16px;
+        padding-bottom: 16px;
+        padding-left: 32px;
+        padding-right: 32px;
+        -webkit-transition: all 0.2s ease-in-out;
+        transition: all 0.2s ease-in-out;
+      }
+
+      <li
+        className="emotion-0"
+      >
+        Item
+      </li>
+    `)
+  })
+
+  it('should render MenuListItem with ReactElement as children', () => {
+    const component = renderer
+      .create(
+        <MenuListItem>
+          <div>Item</div>
+        </MenuListItem>,
+      )
+      .toJSON()
+
+    expect(component).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        cursor: pointer;
+        line-height: 1;
+        padding-top: 16px;
+        padding-bottom: 16px;
+        padding-left: 32px;
+        padding-right: 32px;
+        -webkit-transition: all 0.2s ease-in-out;
+        transition: all 0.2s ease-in-out;
+      }
+
+      <li
+        className="emotion-0"
+      >
+        <div>
+          Item
+        </div>
+      </li>
+    `)
+  })
+
+  it('should render MenuListItem with custom styling', () => {
+    const component = renderer
+      .create(
+        <MenuListItem sx={{ cursor: 'none' }}>
+          <div>Item</div>
+        </MenuListItem>,
+      )
+      .toJSON()
+
+    expect(component).toMatchInlineSnapshot(`
+      .emotion-0 {
+        box-sizing: border-box;
+        margin: 0;
+        min-width: 0;
+        cursor: pointer;
+        line-height: 1;
+        padding-top: 16px;
+        padding-bottom: 16px;
+        padding-left: 32px;
+        padding-right: 32px;
+        -webkit-transition: all 0.2s ease-in-out;
+        transition: all 0.2s ease-in-out;
+        cursor: none;
+      }
+
+      <li
+        className="emotion-0"
+      >
+        <div>
+          Item
+        </div>
+      </li>
+    `)
+  })
+})
