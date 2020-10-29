79b1412aeaec3ac296de69156df28c48e6f83a8d
Alex Chan <alex@skyverge.com>
2020-10-29 15:12:18 -0600

    storybook ref

--- packages/storybook/src/primitives/Box/Box.stories.tsx
+++ packages/storybook/src/primitives/Box/Box.stories.tsx
@@ -17,17 +17,28 @@ export function initial() {
   )
 }
 
-export function asProp() {
+export function AsProp() {
+  const aRef = React.useRef<HTMLAnchorElement>(null)
   return (
-    <Box
-      as="main"
-      sx={{
-        fontFamily: 'body',
-        fontWeight: 'bold',
-        padding: 5,
-      }}
-    >
-      Renders the Box as a Main HTML element
+    <Box>
+      <Box
+        as="main"
+        sx={{
+          fontFamily: 'body',
+          fontWeight: 'bold',
+          padding: 5,
+        }}
+      >
+        Renders the Box as a Main HTML element
+      </Box>
+      <Box
+        as="a"
+        href="#"
+        sx={{ textDecoration: 'underline', padding: 5 }}
+        ref={aRef}
+      >
+        This is a Box as an anchor tag and a Ref
+      </Box>
     </Box>
   )
 }
