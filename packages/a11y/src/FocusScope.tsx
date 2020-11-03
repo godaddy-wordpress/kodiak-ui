f6084959fc951e665ad0b953b44e76036630c406
Wes Cole <wesley.cole87@gmail.com>
2020-11-03 14:23:20 -0500

    Remove commented out code

--- packages/a11y/src/FocusScope.tsx
+++ packages/a11y/src/FocusScope.tsx
@@ -1,35 +1,9 @@
 import * as React from 'react'
 import { useLayoutEffect, useRef, RefObject } from 'react'
-// import create from 'zustand'
 import { FocusScopeOptions, FocusScopeProps } from './types'
 import { useFocusAuto } from './useFocusAuto'
 import { useFocusContain } from './useFocusContain'
 import { useFocusRestore } from './useFocusRestore'
-
-// const useFocusScopeStore = create(() => ({
-//   focusNext(options: FocusScopeOptions) {
-//     const node = options.from || document.activeElement
-//     const focusable = getFocusableElementsInScope(scopeRef.current, opts)
-//     let nextNode = focusable.find(
-//       n =>
-//         !!(
-//           node.compareDocumentPosition(n) &
-//           (Node.DOCUMENT_POSITION_FOLLOWING |
-//             Node.DOCUMENT_POSITION_CONTAINED_BY)
-//         ),
-//     )
-//     if (!nextNode && opts.wrap) {
-//       nextNode = focusable[0]
-//     }
-//     if (nextNode) {
-//       nextNode.focus()
-//     }
-//     return nextNode
-//   },
-//   focusPrevious() {
-//     return 'prev'
-//   },
-// }))
 
 const focusableElements = [
   'input:not([disabled]):not([type=hidden])',
