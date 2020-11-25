b17de15f6c202e17b8defdbe03144d225eea5536
Alex Chan <alex@skyverge.com>
2020-11-24 23:06:24 -0700

    read attributes to hoist from options

--- packages/babel-plugin/src/index.ts
+++ packages/babel-plugin/src/index.ts
@@ -3,8 +3,12 @@ export default function (babel) {
   return {
     name: '@kodiak-ui',
     visitor: {
-      JSXAttribute(path) {
-        if (['sx', 'variant'].includes(path.node.name.name)) {
+      JSXAttribute(path, state) {
+        const attributesToHoist = state?.opts?.attributesToHoist || [
+          'sx',
+          'variant',
+        ]
+        if (attributesToHoist.includes(path.node.name.name)) {
           const expressionPath = path.get('value.expression')
           expressionPath.hoist()
         }
