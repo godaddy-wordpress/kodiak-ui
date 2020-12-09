61153591eadf8ea153c8696c54c2983321182ba8
Wes Cole <wesley.cole87@gmail.com>
2020-12-08 21:59:01 -0500

    Add more stories for autocomplete

--- packages/storybook/src/Autocomplete/Autocomplete.stories.tsx
+++ packages/storybook/src/Autocomplete/Autocomplete.stories.tsx
@@ -1,6 +1,20 @@
 import * as React from 'react'
-import { Autocomplete, useAutocompleteStyles } from '@kodiak-ui/autocomplete'
-import { Grid } from '@kodiak-ui/primitives'
+import {
+  Autocomplete,
+  AutocompleteInputButtonProps,
+  AutocompleteInputProps,
+  useAutocompleteStyles,
+} from '@kodiak-ui/autocomplete'
+import {
+  Button,
+  Flex,
+  Grid,
+  Input,
+  InputGroup,
+  SvgIcon,
+  VisuallyHidden,
+} from '@kodiak-ui/primitives'
+import { SharedSx } from 'kodiak-ui'
 
 export default { title: 'Autocomplete' }
 
@@ -211,49 +225,152 @@ const options = [
   'Zambia',
   'Zimbabwe',
 ]
+const styles = useAutocompleteStyles({
+  root: {
+    width: '300px',
+  },
+  label: {
+    fontWeight: 'normal',
+  },
+  listbox: {
+    boxShadow: 'default',
+    border: '1px solid',
+    borderColor: 'gray.2',
+    borderRadius: 'default',
+  },
+  listboxItem: {
+    px: 4,
+    py: 2,
+  },
+  tagButton: {
+    borderLeft: '1px solid',
+    borderColor: 'gray.3',
+    height: '16px',
+    width: '16px',
+    ml: 1,
+    pl: 1,
+  },
+})
 
 export function SingleValue() {
-  const styles = useAutocompleteStyles({
-    root: {
-      width: '300px',
-    },
-    label: {
-      fontWeight: 'normal',
-    },
-    listbox: {
-      boxShadow: 'default',
-      border: '1px solid',
-      borderColor: 'gray.2',
-      borderRadius: 'default',
-    },
-    listboxItem: {
-      px: 4,
-      py: 2,
-    },
-    tagButton: {
-      borderLeft: '1px solid',
-      borderColor: 'gray.3',
-      height: '16px',
-      width: '16px',
-      ml: 1,
-      pl: 1,
-    },
-  })
-
   return (
-    <Grid sx={{ gridTemplateColumns: 1, gap: 4 }}>
+    <Autocomplete
+      label="Single value"
+      placeholder="Start typing..."
+      options={options}
+      styles={styles}
+    />
+  )
+}
+
+export function MultiValue() {
+  return (
+    <Autocomplete
+      isMulti
+      label="Multiple values"
+      placeholder="Start typing..."
+      options={options}
+      styles={styles}
+    />
+  )
+}
+
+export function Disabled() {
+  return (
+    <Autocomplete
+      isDisabled={true}
+      label="Disabled"
+      placeholder="Start typing..."
+      options={options}
+      styles={styles}
+    />
+  )
+}
+
+export function Clearable() {
+  return (
+    <Grid
+      sx={{
+        gridTemplateColumns: 1,
+        gap: 4,
+      }}
+    >
       <Autocomplete
-        label="Single value"
+        label="Clearable"
         placeholder="Start typing..."
         options={options}
         styles={styles}
       />
       <Autocomplete
-        isMulti
-        label="Multiple values"
+        isClearable={false}
+        label="Not clearable"
         placeholder="Start typing..."
         options={options}
         styles={styles}
+        renderInput={(
+          inputProps: AutocompleteInputProps,
+          clearButtonProps: AutocompleteInputButtonProps,
+          popoverButtonProps: AutocompleteInputButtonProps,
+        ) => (
+          <InputGroup
+            sx={{
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              position: 'relative',
+              pl: '2px',
+              pr: '56px',
+              ...styles?.inputGroup,
+            }}
+          >
+            <Input
+              type="text"
+              variant="shadow"
+              placeholder="Start typing..."
+              {...inputProps}
+              sx={{
+                px: 1,
+                py: '2px',
+                width: 0,
+                minWidth: '30px',
+                flexGrow: 1,
+                textOverflow: 'ellipsis',
+                ...styles?.input,
+              }}
+            />
+            <Flex
+              sx={{
+                alignItems: 'center',
+                position: 'absolute',
+                right: 0,
+                pr: 1,
+                top: '50%',
+                transform: 'translateY(-50%)',
+              }}
+            >
+              <SharedSx
+                sx={{
+                  p: 1,
+                }}
+              >
+                <Button
+                  variants="shadow"
+                  {...popoverButtonProps}
+                  sx={{
+                    height: '20px',
+                    p: 0,
+                    ...styles?.button,
+                  }}
+                >
+                  <SvgIcon height="20" viewBox="0 0 24 24" width="20">
+                    <path d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 10l5 5 5-5z" fill="currentColor" />
+                  </SvgIcon>
+                  <VisuallyHidden>Toggle the list of options</VisuallyHidden>
+                </Button>
+              </SharedSx>
+            </Flex>
+          </InputGroup>
+        )}
       />
     </Grid>
   )
