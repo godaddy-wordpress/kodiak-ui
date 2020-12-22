cbc80aa974a9b62d0ada5c885a5ae97639162e5b
Wes Cole <wesley.cole87@gmail.com>
2020-12-22 10:28:35 -0500

    Add comment for merge

--- packages/kodiak-ui/src/color-mode.ts
+++ packages/kodiak-ui/src/color-mode.ts
@@ -1,6 +1,12 @@
 import { get, merge } from '.'
 import { Theme } from './types'
 
+/**
+ * Accepts a mode name and will look for it in the theme
+ * and merge the mode and base theme together
+ *
+ * @param mode string
+ */
 export function applyMode(mode: string) {
   return function (theme: Theme) {
     if (!mode) {
